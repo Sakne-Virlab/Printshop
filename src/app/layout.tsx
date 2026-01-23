4f72749682692c00e753255c352e6d313deb9be1
Ser0jka123 <sersur42@yandex.ru>
2026-01-23 17:06:31 +0700

    upd

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,8 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.scss";
-import Header from "./components/layout/Header/Header";
-import Footer from "./components/layout/Footer/Footer";
+import Header from "@/components/layout/Header/Header";
+import Footer from "@/components/layout/Footer/Footer";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,9 @@
-import Hero from "./components/mainPage/Hero/Hero";
-import RoadMap from "./components/mainPage/RoadMap/RoadMap";
-import Products from "./components/mainPage/Products/Products";
-import Calculator from "./components/mainPage/Calculator/Calculator";
-import About from "./components/mainPage/About/About";
-import Contacts from "./components/mainPage/Contacts/Contacts";
+import Hero from "@/components/mainPage/Hero/Hero";
+import RoadMap from "@/components/mainPage/RoadMap/RoadMap";
+import Products from "@/components/mainPage/Products/Products";
+import Calculator from "@/components/mainPage/Calculator/Calculator";
+import About from "@/components/mainPage/About/About";
+import Contacts from "@/components/mainPage/Contacts/Contacts";
 
 
 export default function Home() {
